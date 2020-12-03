de416ac8cdb921b7de44acacb07f25355ba093ae
Farshad Amiri <farshad94.am@gmail.com>
2020-12-03 17:12:12 +0330

    instructions added to Prediction_decoder.py

--- Prediction_Decoder (NN output).py
+++ Prediction_Decoder (NN output).py
@@ -4,6 +4,9 @@ def Prediction_decoder(p,n,first_variable,second_variable):
   # first_variable  = you should enter the column (ex. train_df.color) -  doesn't matter test or train
   # second_variable = you should enter the column (ex. train_df.marker) - doesn't matter test or train
 
+  # It works for decoding one hot encoded classification output of neural nets for two separate target variables
+  # for ex. Output = [0 0 0 1 0 0 0 1 0], color = Output[0:6] = [0 0 0 1 0 0] , marker = Output [6:] = [0 1 0]   
+
   first = p[0][:n]
   second = p[0][n:]
 
